18c234967c2f66169a0d904b9d18504d4e91d0fb
EC2 Default User <ec2-user@ip-10-0-0-80.ec2.internal>
2018-01-17 19:02:37 +0000

    Fully moved request form to requests REST resource.

--- payment-requests.js
+++ payment-requests.js
@@ -5,7 +5,7 @@ exports.get = function(event, context) {
  var formContents = fs.readFileSync("public/payment-request-form.html");
  context.succeed({
   statusCode: 200,
-  headers: { 'Content-Type': 'text/html' }
+  headers: { 'Content-Type': 'text/html' },
   body: formContents.toString()
  });
 };
