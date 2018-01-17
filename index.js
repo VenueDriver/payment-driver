67a120cdcc3b4b9c38010a76c38cd12512757580
EC2 Default User <ec2-user@ip-10-0-0-80.ec2.internal>
2018-01-17 18:54:58 +0000

    Basic Twitter Bootstrap payment request form.
    PAY-3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 
  exports.get = function(event, context) {
-   var contents = fs.readFileSync("public/index.html");
+   var contents = fs.readFileSync("public/payment-request-form.html");
    context.succeed({
      statusCode: 200,
      body: contents.toString(),
---
+++ payment-requests.js
@@ -0,0 +1,11 @@
+'use strict';
+var fs = require('fs');
+
+exports.get = function(event, context) {
+ var formContents = fs.readFileSync("public/payment-request-form.html");
+ context.succeed({
+  statusCode: 200,
+  headers: { 'Content-Type': 'text/html' }
+  body: formContents.toString()
+ });
+};
