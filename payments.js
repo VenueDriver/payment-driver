e10a8e02130ac6e7a011d467d1d401e619d0f383
Ryan Alyn Porter <rap@endymion.com>
2018-04-16 19:22:52 +0000

    Payment email notifications.
    PAY-38

--- payments.js
+++ payments.js
@@ -5,6 +5,10 @@ const mustache = require('mustache')
 const AWS = require('aws-sdk')
 const partials = require('./partial-html-templates')
 const PaymentRequest = require('./lib/payment-request.js').PaymentRequest
+const EmailNotification = require('./lib/email-notification.js').EmailNotification
+
+// The company name from the settings, for the email notifications.
+const company = process.env.COMPANY_NAME
 
 // Stripe for payments.
 const keyPublishable = process.env.STRIPE_PUBLISHABLE_KEY
@@ -17,12 +21,11 @@ exports.get = async function (event, context) {
   try {
     var paymentRequest = await PaymentRequest.get(event['queryStringParameters']['id'])
 
-    var templateParameters = {
-      'stripe_publishable_key': keyPublishable,
-      'amount': paymentRequest.amount,
-      'integer_amount': paymentRequest.amount * 100,
-      'description': paymentRequest.description,
-    }
+    var templateParameters = paymentRequest
+    templateParameters.stripe_publishable_key = keyPublishable
+    templateParameters.amount = paymentRequest.amount
+    templateParameters.integer_amount = paymentRequest.amount * 100
+    templateParameters.description = paymentRequest.description
 
     var template = fs.readFileSync('templates/payment-form.mustache', 'utf8')
     var html = mustache.render(template, templateParameters, partials())
@@ -50,13 +53,13 @@ exports.get = async function (event, context) {
 
 // Process a payment.
 exports.post = async function (event, context) {
-  const body = querystring.parse(event.body)
-  var amount = body.amount
-  var stripeToken = body.stripeToken
+  const params = querystring.parse(event.body)
+  var amount = params.amount
+  var stripeToken = params.stripeToken
 
   try {
     var charge = await stripe.charges.create({
-      amount: body.amount,
+      amount: params.amount,
       description: "Sample Charge",
       currency: "usd",
       source: stripeToken
@@ -64,6 +67,60 @@ exports.post = async function (event, context) {
 
     console.log("CHARGE:")
     console.log(JSON.stringify(charge))
+
+    var paymentRequest;
+    try {
+      paymentRequest = await PaymentRequest.get(params.payment_request_id)
+    }
+    // TODO: DRY THIS
+    catch (error) {
+      var parameters = { 'error': error }
+
+      var template = fs.readFileSync('templates/error.mustache', 'utf8')
+      var html = mustache.render(template, parameters, partials())
+
+      return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'text/html' },
+        body: html.toString()
+      }
+    }
+
+    var templateParameters = paymentRequest
+
+    // Add 'Origin' from API Gateway so that the email can include a URL
+    // back to this same instance of the web app.
+    templateParameters.origin = event['headers']['Origin']
+
+    // This notification goes to the customer.
+    templateParameters.subject = "Payment to " + company
+    templateParameters.to = paymentRequest.email
+    var templateName = 'payment-email-to-customer'
+    EmailNotification.sendEmail(templateName, templateParameters,
+      function (error, data) {
+        // If something goes wrong, print an error message.
+        if (error) {
+          console.log(error.message);
+        }
+        else {
+          console.log("Email sent to customer. Message ID: ", data.MessageId);
+        }
+      })
+
+    // This notification goes to the requestor.
+    templateParameters.subject = "Payment from " + paymentRequest.email
+    templateParameters.to = paymentRequest.requestor
+    templateName = 'payment-email-to-requestor'
+    EmailNotification.sendEmail(templateName, templateParameters,
+      function (error, data) {
+        // If something goes wrong, print an error message.
+        if (error) {
+          console.log(error.message);
+        }
+        else {
+          console.log("Email sent to requestor. Message ID: ", data.MessageId);
+        }
+      })
 
     var template = fs.readFileSync('templates/payment-confirmation.mustache', 'utf8')
     var html = mustache.render(template, parameters, partials())
