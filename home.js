8aad4074d7d56669b96e01acab311d14998971dd
Ryan Alyn Porter <rap@endymion.com>
2018-04-18 19:11:45 +0000

    Added a home function on /.
    Is this even possible?  I'm committing this so that I can try it in production to learn more.

---
+++ home.js
@@ -0,0 +1,26 @@
+'use strict'
+const fs = require('fs')
+const querystring = require('querystring')
+const uuidv1 = require('uuid/v1')
+const mustache = require('mustache')
+const moment = require('moment')
+const AWS = require('aws-sdk')
+require('dotenv').load()
+// var passport = require('passport')
+const partials = require('./partial-html-templates')
+const PaymentRequest = require('./lib/payment-request.js').PaymentRequest
+const EmailNotification = require('./lib/email-notification.js').EmailNotification
+
+// The company name from the settings, for the email notifications.
+const company = process.env.COMPANY_NAME
+
+exports.index = async function (event, context) {
+  var template = fs.readFileSync('templates/home.mustache', 'utf8')
+  var html = mustache.render(template, {}, partials())
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/html' },
+    body: html.toString()
+  }
+}
