985cb38cc450ba4c7e948201e5859a9678e9f2fa
GasparErrobidart <podmaster@hotmail.com>
2019-02-11 16:22:09 -0300

    add env vars

--- sam-launchpad.config.js
+++ sam-launchpad.config.js
@@ -6,7 +6,13 @@ module.exports = {
   "project_name" : "tmp-payment-driver",
   "projects" : join( __dirname , "./projects" ),
   "template_parameters" : {
-    "ProjectId" : "1234abc"
+    "ProjectId" : "1234abc",
+    "COMPANY_NAME": process.env.COMPANY_NAME,
+    "SENDER_EMAIL": process.env.SENDER_EMAIL,
+    "CLIENT_ID": process.env.CLIENT_ID,
+    "USER_POOL_ID": process.env.USER_POOL_ID,
+    "STRIPE_SECRET_KEY": process.env.STRIPE_SECRET_KEY,
+    "STRIPE_PUBLISHABLE_KEY": process.env.STRIPE_PUBLISHABLE_KEY
   },
   "commands" : {
     "build" : "npm i && npm run build",
