509cf09a1b0fbf78aabde0f43acb0ae990062c91
GasparErrobidart <podmaster@hotmail.com>
2019-02-11 17:25:26 -0300

    Fixed deployment

--- sam-launchpad.config.js
+++ sam-launchpad.config.js
@@ -8,11 +8,11 @@ module.exports = {
   "template_parameters" : {
     "ProjectId" : "1234abc",
     "CompanyName": process.env.COMPANY_NAME || "local",
-    "SENDER_EMAIL": process.env.SENDER_EMAIL || "local",
-    "CLIENT_ID": process.env.CLIENT_ID || "local",
-    "USER_POOL_ID": process.env.USER_POOL_ID || "local",
-    "STRIPE_SECRET_KEY": process.env.STRIPE_SECRET_KEY || "local",
-    "STRIPE_PUBLISHABLE_KEY": process.env.STRIPE_PUBLISHABLE_KEY || "local"
+    "SenderEmail": process.env.SENDER_EMAIL || "local",
+    "ClientId": process.env.CLIENT_ID || "local",
+    "UserPoolId": process.env.USER_POOL_ID || "local",
+    "StripeSecretKey": process.env.STRIPE_SECRET_KEY || "local",
+    "StripePublishableKey": process.env.STRIPE_PUBLISHABLE_KEY || "local"
   },
   "commands" : {
     "build" : "npm i && npm run build",
