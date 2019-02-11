fbccf27f2a06396c3f089c9979e25877e9a2c25e
GasparErrobidart <podmaster@hotmail.com>
2019-02-11 17:12:10 -0300

    adding to pipeline

--- sam-launchpad.config.js
+++ sam-launchpad.config.js
@@ -7,12 +7,12 @@ module.exports = {
   "projects" : join( __dirname , "./projects" ),
   "template_parameters" : {
     "ProjectId" : "1234abc",
-    "COMPANY_NAME": process.env.COMPANY_NAME,
-    "SENDER_EMAIL": process.env.SENDER_EMAIL,
-    "CLIENT_ID": process.env.CLIENT_ID,
-    "USER_POOL_ID": process.env.USER_POOL_ID,
-    "STRIPE_SECRET_KEY": process.env.STRIPE_SECRET_KEY,
-    "STRIPE_PUBLISHABLE_KEY": process.env.STRIPE_PUBLISHABLE_KEY
+    "CompanyName": process.env.COMPANY_NAME || "local",
+    "SENDER_EMAIL": process.env.SENDER_EMAIL || "local",
+    "CLIENT_ID": process.env.CLIENT_ID || "local",
+    "USER_POOL_ID": process.env.USER_POOL_ID || "local",
+    "STRIPE_SECRET_KEY": process.env.STRIPE_SECRET_KEY || "local",
+    "STRIPE_PUBLISHABLE_KEY": process.env.STRIPE_PUBLISHABLE_KEY || "local"
   },
   "commands" : {
     "build" : "npm i && npm run build",
