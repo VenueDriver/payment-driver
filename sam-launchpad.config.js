d5a78af554f78b47e3a86b38a2ba260a17c5de7e
Ryan Alyn Porter <rap@endymion.com>
2019-02-13 16:04:43 +0000

    I misunderstood the new SAM Launchpad settings at first.  Fixed.

--- sam-launchpad.config.js
+++ sam-launchpad.config.js
@@ -5,7 +5,6 @@ const afterDeployHook = require('./sam-hooks/after-deploy');
 module.exports = {
   "project_name": "payment-driver",
   "single_project": true,
-  "project": join(__dirname, "./lib"),
   "base_path": join(__dirname, "./lib"),
   "template_parameters": {
     "ProjectId": "1234abc",
