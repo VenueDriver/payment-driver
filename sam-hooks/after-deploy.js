70a8eec16b4948c4720a67b76b9dbe29327a3154
Ryan Alyn Porter <rap@endymion.com>
2019-02-13 15:50:16 +0000

    Refactored SAM Launchpad post-install S3 asset hook from projects/core to lib/core.
    Something that I missed when I did the restructuring in the last commit.

--- sam-hooks/after-deploy.js
+++ sam-hooks/after-deploy.js
@@ -1,27 +1,28 @@
-const getStack      = require('../node_modules/sam-launchpad/scripts/get-stack');
-const exec          = require('child_process').exec;
-const getStackName  = require('../util/get-stack-name');
-const join          = require('path').join;
+const getStack = require('../node_modules/sam-launchpad/scripts/get-stack');
+const exec = require('child_process').exec;
+const getStackName = require('../util/get-stack-name');
+const join = require('path').join;
 
 // Hook script must return a promise
-const syncAssets = (options)=>{
-  return new Promise(async (resolve,reject)=>{
+const syncAssets = (options) => {
+  return new Promise(async(resolve, reject) => {
     console.log("  DEPLOYING ASSETS TO S3:");
-    const {args} = options;
+    const { args } = options;
     const appName = "core";
-    const stackName = getStackName(options,appName);
+    const stackName = getStackName(options, appName);
     const stack = await getStack(stackName);
-    const projectPath = "projects/core";
-    const publicDir = join(__dirname , `../${projectPath}/public`);
+    const projectPath = "lib/core";
+    const publicDir = join(__dirname, `../${projectPath}/public`);
     const bucketName = stack.Outputs
-                            .find(data => data.OutputKey == "AssetsLogicAddress")
-                            .OutputValue;
+      .find(data => data.OutputKey == "AssetsLogicAddress")
+      .OutputValue;
     exec(`aws s3 sync ${publicDir} s3://${bucketName}/${appName}/ --acl public-read`,
       (error, stdout, stderr) => {
-        if(error){
+        if (error) {
           console.log(stderr);
           reject(stderr);
-        }else{
+        }
+        else {
           console.log("  done.");
           resolve();
         }
