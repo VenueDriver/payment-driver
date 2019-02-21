8986a3d6dea6462357f726d57a104cdf50e70c43
Gaspar Errobidart <gaspar@bytery.com>
2019-02-21 13:27:19 -0300

    Base Handler tested in local

--- home.js
+++ home.js
@@ -1,7 +1,8 @@
 'use strict'
 const fs = require('fs')
-const template = require('./lib/TemplateRenderer/index');
-const Response = require('./lib/Response/index');
+const template = require('./lib/TemplateRenderer/index')
+const Response = require('./lib/Response/index')
+const BaseHandler = require('./lib/BaseHandler/index')
 const querystring = require('querystring')
 const uuidv1 = require('uuid/v1')
 const mustache = require('mustache')
@@ -32,88 +33,122 @@ const clientId = process.env.CLIENT_ID
 const authenticator = new CognitoAuthenticator(region, userPoolId, clientId)
 const authorizer = new APIGatewayAuthorizer()
 
-exports.index = async function (event, context) {
-  // Check for the access token cookie and verify it if it exists.
-  var accessToken
-  try {
-    accessToken = await authorizer.getValidAccessTokenFromCookie(event)
-  }
-  catch (error) {
-    // Check for new-password
-    console.log("NEW PASSWORD FORM")
-    return loginFormResponse(event, {});
-  }
 
-  if (!accessToken) {
-    // Respond with the login form so that the user can provide their
-    // authentication credentials.
-    return new Response('html').send(
-      await template.render('login', event)
-    );
 
-  }
 
-  return redirectToPaymentRequestsResponse(event)
-}
 
-exports.login = async function (event, context) {
-  const params = querystring.parse(event.body)
 
-  var authResponse
-  try {
-    authResponse = await authenticator.authenticate({
-      username: params.username,
-      password: params.password
-    })
+// * ====================================== *
+// * HANDLERS
+// * ====================================== *
 
-    console.log("Successful Authentication.")
 
-    if (authResponse.ChallengeName == 'NEW_PASSWORD_REQUIRED') {
-      if (params.new_password) {
-        console.log("last response: " + JSON.stringify(authResponse))
-        authResponse = await authenticator.respondToAuthChallenge(authResponse, params.username, params.new_password)
-        console.log("response: " + JSON.stringify(authResponse))
-      }
-      else {
-        return loginFormResponse(event, {
-          'message': 'Please change your password to proceed.',
-          'new_password': true, // Show the 'new password' field.
-          'username': params.username,
-          'password': params.password
-        })
-      }
+
+
+
+
+// Index handler
+
+let indexHandler = new BaseHandler("index").willDo(
+  async function (event, context) {
+    // Check for the access token cookie and verify it if it exists.
+    var accessToken
+    try {
+      accessToken = await authorizer.getValidAccessTokenFromCookie(event)
+    }
+    catch (error) {
+      // Check for new-password
+      console.log("NEW PASSWORD FORM")
+      return loginFormResponse(event, {});
+    }
+    if (!accessToken) {
+      // Respond with the login form so that the user can provide their
+      // authentication credentials.
+      return new Response('html').send(
+        await template.render('login')
+      );
+
     }
 
-    console.log("Verifying access token: " + authResponse.AuthenticationResult.AccessToken)
-    await authenticator.verifyCognitoToken(authResponse.AuthenticationResult.AccessToken)
-    return redirectToPaymentRequestsResponse(event, authResponse.AuthenticationResult.AccessToken)
+    return redirectToPaymentRequestsResponse(event)
   }
-  catch (error) {
-    return loginFormResponse(event, {
-      'message': error,
-      'username': params.username,
-      'password': params.password
-    })
-  }
-}
+);
 
-exports.logout = async function (event, context) {
-  return {
-    statusCode: 302,
-    headers: {
-      location: 'https://' + event.headers.Host + '/',
-      // Remove the access token cookie.
-      'Set-Cookie': 'access_token=; Expires=Mon, 30 Apr 2012 22:00:00 EDT'
+
+
+
+
+
+// Login handler
+let loginHandler = new BaseHandler("login").willDo(
+  async function (event, context) {
+    const params = querystring.parse(event.body)
+
+    var authResponse
+    try {
+      authResponse = await authenticator.authenticate({
+        username: params.username,
+        password: params.password
+      })
+
+      console.log("Successful Authentication.")
+
+      if (authResponse.ChallengeName == 'NEW_PASSWORD_REQUIRED') {
+        if (params.new_password) {
+          console.log("last response: " + JSON.stringify(authResponse))
+          authResponse = await authenticator.respondToAuthChallenge(authResponse, params.username, params.new_password)
+          console.log("response: " + JSON.stringify(authResponse))
+        }
+        else {
+          return loginFormResponse(event, {
+            'message': 'Please change your password to proceed.',
+            'new_password': true, // Show the 'new password' field.
+            'username': params.username,
+            'password': params.password
+          })
+        }
+      }
+
+      console.log("Verifying access token: " + authResponse.AuthenticationResult.AccessToken)
+      await authenticator.verifyCognitoToken(authResponse.AuthenticationResult.AccessToken)
+      return redirectToPaymentRequestsResponse(event, authResponse.AuthenticationResult.AccessToken)
+    }
+    catch (error) {
+      return loginFormResponse(event, {
+        'message': error,
+        'username': params.username,
+        'password': params.password
+      })
     }
   }
-}
+);
+
+
+
+
+
+
+// Logout handler
+let logoutHandler = new BaseHandler("logout").willDo(
+  async function (event, context) {
+    return {
+      statusCode: 302,
+      headers: {
+        location: 'https://' + event.headers.Host + '/',
+        // Remove the access token cookie.
+        'Set-Cookie': 'access_token=; Expires=Mon, 30 Apr 2012 22:00:00 EDT'
+      }
+    }
+  }
+);
+
+
 
 function redirectToPaymentRequestsResponse(event, accessToken) {
   var headers = { location: 'https://' + event.headers.Host + '/payment-requests' }
   if (accessToken) {
     // This is a session cookie, since it has no expiration set.
-    headers['Set-Cookie'] =
-      'access_token = ' + accessToken + "; Secure; SameSite=Strict"
+    headers['Set-Cookie'] = 'access_token = ' + accessToken + "; Secure; SameSite=Strict"
   }
   console.log("Redirecting: " + JSON.stringify(headers))
   return {
@@ -121,3 +156,11 @@ function redirectToPaymentRequestsResponse(event, accessToken) {
     headers: headers
   }
 }
+
+// * ====================================== *
+// * EXPORTS
+// * ====================================== *
+
+exports.index   = indexHandler.do;
+exports.login   = loginHandler.do;
+exports.logout  = logoutHandler.do;
