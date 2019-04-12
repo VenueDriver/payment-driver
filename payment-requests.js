db3528aafd37b2bb7f395071a888f4e0c37f3339
Gaspar Errobidart <gaspar@bytery.com>
2019-04-12 16:28:27 -0300

    TemplateRenderer support sub directories

--- payment-requests.js
+++ payment-requests.js
@@ -19,7 +19,7 @@ const company = process.env.COMPANY_NAME
 let indexHandler = new BaseHandler("index").willDo(
   async function (event, context) {
     var templateParameters
-  
+
     try {
       // If a payment request ID and created_at time stamp was provided as
       // parameters, then show that payment request instead of the list.
@@ -39,7 +39,7 @@ let indexHandler = new BaseHandler("index").willDo(
       }
       else {
         var paymentRequests = await PaymentRequest.index()
-        
+
         // Sort them in mempory.
         // (Since it's so hard to sort them with DynamoDB.)
         paymentRequests.sort((a,b)=>{
@@ -57,7 +57,7 @@ let indexHandler = new BaseHandler("index").willDo(
             return moment(this.created_at).fromNow()
           }
         }
-        
+
         return new Response('200').send(
           await template.render('payment-requests', templateParameters))
       }
@@ -82,23 +82,23 @@ let postHandler = new BaseHandler("post").willDo(
     // Create the payment request record
     var paymentRequest = querystring.parse(event.body)
     paymentRequest['id'] = uuidv1()
-  
+
     try {
       await PaymentRequest.put(paymentRequest)
       var templateParameters = paymentRequest
-  
+
       // This notification goes to the customer.
       templateParameters.subject = "Payment request from " + company
       templateParameters.to = paymentRequest.email
       var templateName = 'payment-request-email-to-customer'
       await EmailNotification.sendEmail(templateName, global.handler.base_url, templateParameters)
-  
+
       // This notification goes to the requestor.
       templateParameters.subject = "Payment request to " + paymentRequest.email
       templateParameters.to = paymentRequest.requestor
       templateName = 'payment-request-email-to-requestor'
       await EmailNotification.sendEmail(templateName, templateParameters)
-  
+
       return new Response('200').send(
         await template.render('payment-request-confirmation', templateParameters))
     }
