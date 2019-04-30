7b8a48ea431c69eda8c3eb091d92602567198b4b
Gaspar Errobidart <gaspar@bytery.com>
2019-04-30 15:55:19 -0300

    Update reject-if-paid.js
    -fix error message

--- middleware/reject-if-paid.js
+++ middleware/reject-if-paid.js
@@ -7,7 +7,7 @@ async function rejectIfPaid(event, context) {
 
   if(paymentRequest.payment && paymentRequest.payment.captured){
     return new Response('200').send(
-      await template.render('error', { 'error': 'This payment request is no longer available, please <a href="mailto:support@venuedriver.com">contact support.</a>' }))
+      await template.render('error', { 'error': 'This payment request is no longer available, please contact support at <a href="mailto:support@venuedriver.com">support@hakkasan.com</a>.' }))
   }
 
   global.handler.customer_facing = true;
