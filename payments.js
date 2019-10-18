3a93c5ad2a6d3df45581303cfdb9fa510ec48c9b
GasparErrobidart <podmaster@hotmail.com>
2019-10-18 14:06:54 -0300

    Update payments.js

--- payments.js
+++ payments.js
@@ -13,6 +13,7 @@ const BigNumber = require('bignumber.js');
 const Hook      = require('./lib/Hook')
 
 
+
 const setCustomerFacing = require('./middleware/customer-endpoint');
 const loadPaymentRequest = require('./middleware/load-existing-payment-request');
 const rejectIfPaid = require('./middleware/reject-if-paid');
