f781a66b5ebe25052d7cfdaa13fa4ae30bb4bec2
Franco David Marsili del Grosso <francomarsili@bytery.com>
2020-02-12 14:36:25 -0300

    Check if the token has been succesfully decoded to be able to access to the get and post of the payment request form

--- middleware/bypass-new-payment-request-authenticator.js
+++ middleware/bypass-new-payment-request-authenticator.js
@@ -1,18 +1,28 @@
 const PaymentRequestAuthorizer = require('../lib/PaymentRequestAuthorizer');
+const Response = require('../lib/Response')
 
 async function bypassNewPaymentRequestAuthenticator(event, context) {
   console.log("\nBypassNewPaymentRequestAuthenticator\n");
   const authorizer = new PaymentRequestAuthorizer();
 
+  let paymentRequestRequestPayload = null;
+
   try {
     let accessToken = authorizer.getValidAccessTokenFromQueryParams(event);
-    global.handler.paymentRequestRequestPayload = authorizer.decode(accessToken);
-    console.log("Token Payload:",global.handler.paymentRequestRequestPayload);
-    global.handler.queryParams = `?payment-request-token=${accessToken}`;
-    global.handler.skipAuthentication = true;
+    paymentRequestRequestPayload = authorizer.decode(accessToken);
+    if(paymentRequestRequestPayload){
+      global.handler.paymentRequestRequestPayload = paymentRequestRequestPayload;
+      console.log("Token Payload:",global.handler.paymentRequestRequestPayload);
+      global.handler.queryParams = `?payment-request-token=${accessToken}`;
+      global.handler.skipAuthentication = true;
+    }
   }
   catch (error) {
     console.log(error);
+  }
+
+  if(!paymentRequestRequestPayload){
+    return new Response('302').redirect('payment-requests')
   }
 }
 
